66d954fc56d19a96fd5d19ce8a6d2356816f4fbd
ЛЮБОМИР САКАЛЮК <[cj2222cj2222@gmail.com]>
2022-05-07 16:26:29 +0300

    first commit

---
+++ program_one.py
@@ -0,0 +1,16 @@
+import os
+import time 
+
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://www.google.com/') 
+
+time.sleep(3)
+
+os.startfile('C:Users/User/Pictures')
---
+++ program_one2.py
@@ -0,0 +1,11 @@
+import os
+import time 
+
+os.system('start ' + 'https://www.google.com/')
+os.system('start ' + 'https://pypi.org/search/?q=watchdog&o=/')
+os.system('start ' + 'https://https://github.com/')
+
+
+time.sleep(3)
+
+os.startfile('C:Users/User/Pictures')
